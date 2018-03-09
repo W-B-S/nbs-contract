26c13a2979c545405983965742e430baaf2131e0
W-B-S <lws99927800@163.com>
2018-03-09 21:50:07 +0800

    init

--- migrations/2_ipmcoin_migration.js
+++ migrations/2_ipmcoin_migration.js
@@ -1,7 +1,12 @@
 var IPMCoin = artifacts.require("./IPMCoin.sol");
+var ConvertLib = artifacts.require("./ConvertLib.sol");
 
 module.exports = function(deployer) {
   deployer.deploy(IPMCoin).then(function(instance){
   	console.log(instance);
   });
+
+    deployer.deploy(ConvertLib).then(function(instance){
+        console.log(instance);
+    });
 };
--- test/TestIPMCoin.sol
+++ test/TestIPMCoin.sol
@@ -3,15 +3,19 @@ pragma solidity ^0.4.2;
 import "truffle/Assert.sol";
 import "truffle/DeployedAddresses.sol";
 import "../contracts/IPMCoin.sol";
+import "../contracts/ConvertLib.sol";
 
 contract TestIPMCoin {
+    uint8 public decimals = 18;
 
   function testInitialBalanceUsingDeployedContract() public {
-    IPMCoin meta = IPMCoin(DeployedAddresses.IPMCoin());
 
-    uint expected = 1e10;
+        IPMCoin meta = IPMCoin(DeployedAddresses.IPMCoin());
 
-    Assert.equal(meta.getBalanceOf(0x627306090abaB3A6e1400e9345bC60c78a8BEf57), expected, "Owner should have 1e10 IPMCoin initially");
+        uint expected = ConvertLib.convert(1e10, 10 ** uint256(decimals));
+
+      Assert.equal(meta.getBalanceOf(0x627306090abaB3A6e1400e9345bC60c78a8BEf57),
+          expected, "Owner should have 1e10 IPMCoin initially");
   }
 
   function testInitialBalanceWithNewMetaCoin() public {
